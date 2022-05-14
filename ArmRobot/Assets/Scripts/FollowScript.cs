95abdc0afe3c0df2909e45db5aae4dd2c0e5da4d
BP-2 <bp309420@ohio.edu>
2022-05-14 17:12:45 -0400

    update

---
+++ ArmRobot/Assets/Scripts/FollowScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject person;
+    private Vector3 offset = new Vector3(0,.5,.7);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(person.transform.position.x+offset.x,person.transform.position.y+offset.y,person.transform.position.z+offset.z);
+    }
+}
